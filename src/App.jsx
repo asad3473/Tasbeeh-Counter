d70e9d3a3ef48da8de32dd619b1aba0f76585618
asad3473 <asadsoomro0311@gmail.com>
2025-07-13 14:54:32 +0500

    update image path

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <div className='w-full h-[100vh]'>
       <div className='w-full h-full relative'>
-        <img src='src/assets/tasbeeh.gif' className='h-full absolute w-full object-cover' />
+        <img src='/assets/tasbeeh.gif' className='h-full absolute w-full object-cover' />
         <div className="absolute top-0 h-full w-full bg-black/50"></div>
         <div className='absolute top-0 w-full flex justify-content-center flex-col h-full items-center'>
           <h1 className='mb-4 mt-2 text-center text-xl sm:text-3xl font-semibold text-white'>Online Tasbeeh Counter</h1>
@@ -27,7 +27,7 @@ function App() {
             className="h-[300px] w-full sm:h-[300px] sm:w-[400px] bg-no-repeat bg-center bg-contain"
             style={{
               blockSize: '100%',
-              backgroundImage: `url(src/assets/counter.png)`,
+              backgroundImage: `url(/assets/counter.png)`,
             }}
           >
             <div className='w-full h-[90%] flex justify-center items-center'>
